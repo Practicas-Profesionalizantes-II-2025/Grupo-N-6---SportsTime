5c18a9116ed48b6e2c45661f5a48b009ab445e58
Leandro Bolzon <leosilva1711@gmail.com>
2025-09-02 17:37:17 -0300

    .

--- sportime/proyecto - copia/ProyectoSportTime/CDatos/Repositorys/AdministradorRepository.cs
+++ sportime/proyecto - copia/ProyectoSportTime/CDatos/Repositorys/AdministradorRepository.cs
@@ -3,13 +3,8 @@
 //using Newtonsoft.Json;
 using Shared.Dtos;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CDatos.Data;
 using Shared.Entidades;
-using Shared.Dtos;
 using Microsoft.EntityFrameworkCore;
 using CDatos.Repositorys.IRepositorys;
 
